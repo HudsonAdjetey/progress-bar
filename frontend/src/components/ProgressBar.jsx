c78282ce85e6f3e2c019483a3c88cbf855bd6470
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-29 11:00:34 -0700

    import constants

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { MAX, MIN } from "../constants";
 
 const ProgressBar = ({ value = 0 }) => {
     const [percent, setPercent] = useState(value)
 
     useEffect(()=> {
-        setPercent(Math.min(100, Math.max(value, 0)))
+        setPercent(Math.min(MAX, Math.max(value, MIN)))
     },[value])
   return (
     <div className="progress">
