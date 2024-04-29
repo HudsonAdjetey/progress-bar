34a2175cceb965bdc07fa308998d78e585413b5d
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-29 10:44:58 -0700

    second

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ProgressBar from "./components/ProgressBar";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [value, setValue] = useState(0);
 
-export default App
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prev) => prev + 1);
+    }, 100);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div className="app">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} />
+    </div>
+  );
+};
+
+export default App;
