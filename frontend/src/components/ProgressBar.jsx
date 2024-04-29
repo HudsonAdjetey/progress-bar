de7f598b615e4cf1aad74897e7841892272a503d
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-29 11:09:48 -0700

    constants modified

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from "react";
 import { MAX, MIN } from "../constants";
 
 const ProgressBar = ({ value = 0 }) => {
-    const [percent, setPercent] = useState(value)
+  const [percent, setPercent] = useState(value);
 
-    useEffect(()=> {
-        setPercent(Math.min(MAX, Math.max(value, MIN)))
-    },[value])
+  useEffect(() => {
+    setPercent(Math.min(MAX, Math.max(value, MIN)));
+  }, [value]);
   return (
     <div className="progress">
-      <span
-      style={{color: percent > 53 ? "white" : "black"}}
-      >{percent.toFixed()}%</span>
-      <div style={{width: `${percent}%`}} role="progressbar" aria-valuemin={0} aria-valuemax={100} aria-valuenow={percent.toFixed()} ></div>
+      <span style={{ color: percent > 53 ? "white" : "black" }}>
+        {percent.toFixed()}%
+      </span>
+      <div
+        // style={{ width: `${percent}%` }}
+        style={{
+          transform: `scaleX(${percent / MAX})`,
+          transformOrigin: "left",
+        }}
+        role="progressbar"
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-valuenow={percent.toFixed()}
+      ></div>
     </div>
   );
 };
